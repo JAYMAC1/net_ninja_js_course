313931129a4d80a91525fb83dc01495965a7b5c7
James McNeil <mcneil_james@hotmail.com>
2022-05-25 22:31:36 +0100

    two sum challange

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,21 @@
-console.log('linked to html')
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+
+/**
+ * Do not change this function body
+ * Do your changes within the function
+ */
+
+function twoSum(nums, target) {
+	const numbers = [...nums]
+	const sum = numbers.forEach((n) => {
+		n = n + n
+	})
+	console.log(numbers, target, sum)
+	// your code goes here
+}
+
+twoSum([2, 4], 8)
