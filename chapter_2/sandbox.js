c4f74d81510f9a1ae91cd3e2e04e9ec4f46e9f42
James McNeil <mcneil_james@hotmail.com>
2022-05-26 22:22:28 +0100

    strings

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,22 +1,32 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {boolean}
- */
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {boolean}
+//  */
 
-/**
- * Do not change this function body
- * Do your changes within the function
- */
+// /**
+//  * Do not change this function body
+//  * Do your changes within the function
+//  */
 
-function twoSum(nums, target) {
-	const numbers = [...nums]
-	const result = nums[0] + nums[1] === target
-	// const sum = numbers.forEach((n) => {
-	// 	n = n + n
-	// })
-	console.log(numbers, target, result)
-	// your code goes here
-}
+// function twoSum(nums, target) {
+// 	const numbers = [...nums]
+// 	const result = nums[0] + nums[1] === target
+// 	// const sum = numbers.forEach((n) => {
+// 	// 	n = n + n
+// 	// })
+// 	console.log(numbers, target, result)
+// 	// your code goes here
+// }
 
-twoSum([3, 5], 8)
+// twoSum([3, 5], 8)
+
+// let email = 'mario@thenetninja.co.uk'
+
+// let result = email.lastIndexOf('@')
+// let result = email.slice(0, 5)
+// let result = email.substring(5, 10)
+// let result = email.replace('m', 'w')
+// let result = email.replace('n', 'w')
+
+// console.log(result)
