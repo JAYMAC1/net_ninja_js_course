30ab60c5cd9657f7e330e03fbef17e63407ef344
James McNeil <mcneil_james@hotmail.com>
2022-05-29 20:49:11 +0100

    break and Continue

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -80,3 +80,16 @@
 // if (!user) {
 //   console.log('User is not logged in')
 // }
+
+const scores = [50, 25, 0, 30, 100, 20, 10]
+for (let i = 0; i < scores.length; i++) {
+  // if (scores[i] === 0) {
+  //   continue
+  // }
+
+  console.log('your score:', scores[i])
+  if (scores[i] === 100) {
+    console.log('congrats, you got the high score')
+    break
+  }
+}
