fdbcee038f1c989c751ee239b81c7d64f4bfdd1e
James McNeil <mcneil_james@hotmail.com>
2022-05-31 20:47:38 +0100

    functions declaritive ad expression

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,5 +1,14 @@
-// Functions and Methods
-const sums = function (number1, number2) {
-	const result = number1 + number2
-	console.log(result)
-}
+// greet() // works because it is hoisted
+
+// //  function declaration - These are hoisted to the top of the code
+// function greet() {
+//   console.log('Hello')
+// }
+
+// // function expression - Not hoisted
+
+// const speak = function () {
+//   console.log('Goodbye')
+// }
+
+// speak()
