25cfab6286e21065db5064f597a3858adbec1adf
James McNeil <mcneil_james@hotmail.com>
2022-06-02 19:42:05 +0100

    functions and methods

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -43,18 +43,29 @@
 // const area = calcArea(5)
 // console.log('area is', area)
 
-const greet = () => {
-  return 'Hello, world!'
-}
+// const greet = () => {
+//   return 'Hello, world!'
+// }
 
-console.log(greet())
+// console.log(greet())
 
-const bill = (products, tax) => {
-  let total = 0
-  for (let i = 0; i < products.length; i++) {
-    total += products[i] * tax
-  }
-  return total
-}
+// const bill = (products, tax) => {
+//   let total = 0
+//   for (let i = 0; i < products.length; i++) {
+//     total += products[i] * tax
+//   }
+//   return total
+// }
 
-console.log(bill([10, 15, 30], 0.2))
+// console.log(bill([10, 15, 30], 0.2))
+const name = 'James'
+// functions
+
+const greet = () => 'hello'
+let resultOne = greet()
+console.log(resultOne)
+
+// methods
+
+let resultTwo = name.toUpperCase()
+console.log(resultTwo)
