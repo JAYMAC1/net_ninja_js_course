3f734a69d375cca126ea7edeb615f173df906818
James McNeil <mcneil_james@hotmail.com>
2022-07-24 13:57:06 +0100

    callback example

--- 4_section_four/lesson_35.js
+++ 4_section_four/lesson_35.js
@@ -1,1 +1,13 @@
 console.log('Section Four - Functions & Methods')
+
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li']
+
+people.forEach(function (person) {
+  console.log(person)
+})
+
+people.forEach((person) => {
+  console.log(person)
+})
+
+people.forEach((person) => console.log(person))
