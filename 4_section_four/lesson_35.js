76134731015b440126cad481230ef33fd0c3410e
James McNeil <mcneil_james@hotmail.com>
2022-07-24 14:07:24 +0100

    complete

--- 4_section_four/lesson_35.js
+++ 4_section_four/lesson_35.js
@@ -2,12 +2,21 @@ console.log('Section Four - Functions & Methods')
 
 let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li']
 
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`)
+}
+
+// Standard function
 people.forEach(function (person) {
   console.log(person)
 })
 
-people.forEach((person) => {
-  console.log(person)
+// Arrow function
+people.forEach((person, index) => {
+  console.log(index + 1, person)
 })
 
+// shortened Arrow function
 people.forEach((person) => console.log(person))
+
+people.forEach(logPerson)
