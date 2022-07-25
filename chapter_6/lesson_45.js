0bec88fdb366821ecca566af6a3d5a57da2e057a
James McNeil <mcneil_james@hotmail.com>
2022-07-25 07:27:01 +0100

    lesson 49 file missed on commit

--- chapter_6/lesson_45.js
+++ chapter_6/lesson_45.js
@@ -66,3 +66,15 @@ people.forEach((person) => {
 // ########################################################################
 // Lesson 49 - Getting & Setting Attributes
 // ########################################################################
+
+const link = document.querySelector('a')
+
+console.log(link.getAttribute('href'))
+
+link.setAttribute('href', 'https://www.thenetninja.co.uk')
+link.innerText = 'The Net Ninja'
+
+const msg = document.querySelector('p')
+console.log(msg.getAttribute('class'))
+msg.setAttribute('class', 'success')
+msg.setAttribute('style', 'color: green;')
