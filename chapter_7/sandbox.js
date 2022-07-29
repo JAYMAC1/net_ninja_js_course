4624083ce8c3b7d5f8509a39da1dd0c0e6e3e47b
James McNeil <mcneil_james@hotmail.com>
2022-07-29 08:05:17 +0100

    lesson 60 complete

--- chapter_7/sandbox.js
+++ chapter_7/sandbox.js
@@ -12,3 +12,30 @@ form.addEventListener('submit', (e) => {
   console.log(form.username.value)
 })
 */
+
+// ########################################################################
+// Lesson 60 - Regular Expressions
+// ########################################################################
+
+const form = document.querySelector('.signup-form')
+// const username = document.querySelector('#username')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  // console.log(username.value)
+  console.log(form.username.value)
+})
+
+// testing RegEx
+const username = 'jamesmcn'
+const pattern = /^[a-z]{6,}$/
+
+// let result = pattern.test(username)
+// if (result) {
+//   console.log(`regex test passed :) `)
+// } else {
+//   console.log(`input string failed to match`)
+// }
+
+let result = username.search(pattern)
+console.log(result)
