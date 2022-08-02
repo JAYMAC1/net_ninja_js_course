92a2f84fced878d64dbe5360c3441cae17bbb94b
James McNeil <mcneil_james@hotmail.com>
2022-08-02 13:32:59 +0100

    checking answers

--- chapter_08/app.js
+++ chapter_08/app.js
@@ -1,2 +1,19 @@
+console.log('connected')
 const correctAnswers = ['B', 'B', 'B', 'B']
 const form = document.querySelector('.quiz-form')
+
+console.log(form)
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(form.q1.value)
+
+  let score = 0
+  const userAnswers = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ]
+  console.log(userAnswers)
+})
