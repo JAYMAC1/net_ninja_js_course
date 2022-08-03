32c71af25c90d38459bdd6d98f8dc2837e96934c
James McNeil <mcneil_james@hotmail.com>
2022-08-03 23:28:54 +0100

    lesson 67 cmplete

--- chapter_08/app.js
+++ chapter_08/app.js
@@ -1,13 +1,8 @@
-console.log('connected')
 const correctAnswers = ['B', 'B', 'B', 'B']
 const form = document.querySelector('.quiz-form')
 
-console.log(form)
-
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-  console.log(form.q1.value)
-
   let score = 0
   const userAnswers = [
     form.q1.value,
@@ -15,5 +10,13 @@ form.addEventListener('submit', (e) => {
     form.q3.value,
     form.q4.value,
   ]
-  console.log(userAnswers)
+
+  // compare answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]) {
+      score += 25
+    }
+  })
+
+  console.log(score)
 })
