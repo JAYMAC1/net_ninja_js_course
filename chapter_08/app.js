2291fccefe411e427d3ccfe74e41f8b394398b3e
James McNeil <mcneil_james@hotmail.com>
2022-08-15 19:58:28 +0100

    lesson 68 complete display score

--- chapter_08/app.js
+++ chapter_08/app.js
@@ -1,9 +1,11 @@
 const correctAnswers = ['B', 'B', 'B', 'B']
 const form = document.querySelector('.quiz-form')
+const result = document.querySelector('.result')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   let score = 0
+  // Capture form select values into an array of the users answers
   const userAnswers = [
     form.q1.value,
     form.q2.value,
@@ -18,5 +20,7 @@ form.addEventListener('submit', (e) => {
     }
   })
 
-  console.log(score)
+  // Output Score
+  result.querySelector('span').textContent = `${score}%`
+  result.classList.remove('d-none')
 })
