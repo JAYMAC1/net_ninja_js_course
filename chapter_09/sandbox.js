2c6823c730b01448179ad4c02cdc3442fdfb6b0d
James McNeil <mcneil_james@hotmail.com>
2022-08-16 15:34:50 +0100

    Lesson 71 Filter Method complete

---
+++ chapter_09/sandbox.js
@@ -0,0 +1,30 @@
+// ########################################################################
+// Lesson 71 - Filter Method
+// ########################################################################
+
+// const scores = [10.3, 15, 25, 50, 40, 5]
+
+// Array method - filter
+// Filter out items based on a certain condition to a new filtered array
+// i.e all scores over 20
+
+// const filteredScores = scores.filter((score) => {
+//   return score > 20
+// })
+
+// console.log(filteredScores)
+
+// const users = [
+//   { name: 'James', premium: false },
+//   { name: 'Rachel', premium: true },
+//   { name: 'Millie', premium: true },
+//   { name: 'Jess', premium: false },
+// ]
+
+// const filteredUsers = users.filter((user) => user.premium)
+
+// console.log(filteredUsers)
+
+// ########################################################################
+// Lesson 71 - Filter Method
+// ########################################################################
