f4b1aec2737c3f01700666a1343af37d343a6a7f
James McNeil <mcneil_james@hotmail.com>
2022-08-16 17:25:32 +0100

    Lesson 76 and section 9 complete

--- chapter_09/sandbox.js
+++ chapter_09/sandbox.js
@@ -145,3 +145,21 @@
 // players.sort((a, b) => b.score - a.score)
 
 // console.log(players.reverse())
+
+// ########################################################################
+// Lesson 76 - Chanign Array Methods
+// ########################################################################
+
+const products = [
+  { name: 'gold star', price: 30 },
+  { name: 'green shell', price: 10 },
+  { name: 'red shell', price: 40 },
+  { name: 'banana skin', price: 5 },
+  { name: 'mushroom', price: 50 },
+]
+
+const promos = products
+  .filter((product) => product.price > 20)
+  .map((product) => `the ${product.name} is ${product.price / 2} pounds`)
+
+console.log(promos)
