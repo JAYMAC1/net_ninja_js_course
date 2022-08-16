7b83d000f3a3c0637d86c3eabc1579768567a80d
James McNeil <mcneil_james@hotmail.com>
2022-08-16 15:54:23 +0100

    lesson 72 complete

--- chapter_09/sandbox.js
+++ chapter_09/sandbox.js
@@ -26,5 +26,38 @@
 // console.log(filteredUsers)
 
 // ########################################################################
-// Lesson 71 - Filter Method
+// Lesson 72 - Map Method
+// ########################################################################
+
+// Takes array and 'maps' into a completely new array based on the original contents
+// i.e. iterate the prices and for each item calulate a 'sale' price and return to a new array
+
+// const prices = [20, 10, 30, 25, 15, 40, 80, 5]
+
+// const salePrices = prices.map((price) => {
+//   return price / 2
+// })
+
+// console.log(salePrices)
+
+// const products = [
+//   { name: 'gold star', price: 20 },
+//   { name: 'mushroom', price: 40 },
+//   { name: 'green shells', price: 30 },
+//   { name: 'banana skin', price: 10 },
+//   { name: 'red shells', price: 50 },
+// ]
+
+// const saleProducts = products.map((product) => {
+//   if (product.price > 30) {
+//     return { name: product.name, price: (product.price = product.price / 2) }
+//   } else {
+//     return product
+//   }
+// })
+
+// console.log(saleProducts)
+
+// ########################################################################
+// Lesson 73 - Reduce Method
 // ########################################################################
