000e8e49bc291f9a0f2dcd3f9bbebd60acd3d8d8
James McNeil <mcneil_james@hotmail.com>
2022-08-18 21:45:54 +0100

    lesson 81 complete - totdo project complete

--- chapter_10/app.js
+++ chapter_10/app.js
@@ -1,5 +1,7 @@
 const addForm = document.querySelector('.add')
 const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
+
 const generateTemplate = (todo) => {
   const html = `
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -25,4 +27,28 @@ list.addEventListener('click', (e) => {
     console.log(e.target.parentElement)
     e.target.parentElement.remove()
   }
+})
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => {
+      return !todo.textContent.includes(term)
+    })
+    .forEach((todo) => {
+      todo.classList.add('filtered')
+    })
+
+  Array.from(list.children)
+    .filter((todo) => {
+      return todo.textContent.includes(term)
+    })
+    .forEach((todo) => {
+      todo.classList.remove('filtered')
+    })
+}
+
+// search todos
+search.addEventListener('keyup', (e) => {
+  const term = search.value.trim()
+  filterTodos(term)
 })
