74a92bea962412bf1299b644c1779453ba08f736
James McNeil <mcneil_james@hotmail.com>
2022-08-19 22:36:53 +0100

    lesson

--- chapter_11/app.js
+++ chapter_11/app.js
@@ -4,3 +4,10 @@ const now = new Date()
 
 console.log(now)
 console.log(typeof now)
+
+//  years, months days, times
+
+console.log('getFullYear:', now.getFullYear())
+console.log('getMonth:', now.getMonth())
+console.log('getDate:', now.getDate())
+console.log('getDay:', now.getDay())
