b9d3f4645777a339b1738ff988c6d1e71633997a
James McNeil <mcneil_james@hotmail.com>
2022-09-11 18:42:39 +0100

    lesson 82 Dates & Times in JavaScript - complete

--- chapter_11/app.js
+++ chapter_11/app.js
@@ -11,3 +11,14 @@ console.log('getFullYear:', now.getFullYear())
 console.log('getMonth:', now.getMonth())
 console.log('getDate:', now.getDate())
 console.log('getDay:', now.getDay())
+console.log('getHours', now.getHours())
+console.log('getMinutes', now.getMinutes())
+console.log('getSeconds', now.getSeconds())
+
+// Timestamps
+console.log('timestamp', now.getTime())
+
+//  data strings
+console.log(now.toDateString(0))
+console.log(now.toTimeString(0))
+console.log(now.toLocaleString(0))
