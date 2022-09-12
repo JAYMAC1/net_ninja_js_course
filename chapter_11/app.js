9097934985b3f0f1af298a8db01f64cfa75a4192
James McNeil <mcneil_james@hotmail.com>
2022-09-12 22:37:03 +0100

    lesson 83 - Timestamps and Comparisons complete

--- chapter_11/app.js
+++ chapter_11/app.js
@@ -1,3 +1,4 @@
+/*
 // Dates & times
 
 const now = new Date()
@@ -22,3 +23,28 @@ console.log('timestamp', now.getTime())
 console.log(now.toDateString(0))
 console.log(now.toTimeString(0))
 console.log(now.toLocaleString(0))
+*/
+
+// timestamps
+
+const before = new Date('February 1 2019 7:30:59')
+const now = new Date()
+
+const diff = now.getTime() - before.getTime()
+console.log(diff)
+
+const mins = Math.round(diff / 1000 / 60)
+const hours = Math.round(mins / 60)
+const days = Math.round(hours / 24)
+const months = Math.floor(days / 365)
+const years = Math.floor(days / 365)
+
+console.log(`the blog was written ${years} years ago`)
+console.log(mins)
+console.log(hours)
+console.log(days)
+console.log(years)
+
+// converting timestamps into date objects
+const timestamp = 1675938474990
+console.log(new Date(timestamp))
