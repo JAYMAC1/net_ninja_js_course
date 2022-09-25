57f91831708fe698d67b8ffb939bed6522a2433c
James McNeil <mcneil_james@hotmail.com>
2022-09-25 20:48:24 +0100

    XMLHttpRequest test

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,4 +1,10 @@
-console.log(1)
-console.log(2)
-console.log(3)
-console.log(4)
+const request = new XMLHttpRequest()
+
+request.addEventListener('readystatechange', () => {
+  // console.log(request, request.readyState)
+  if (request.readyState === 4) {
+    console.log(request.responseText)
+  }
+})
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+request.send()
