d1b38d3e11386f9293306189d6247209b4f966a4
James McNeil <mcneil_james@hotmail.com>
2022-10-02 16:27:15 +0100

    lesson 97: async await - complete

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,13 +1,36 @@
 // fetch api
 
-fetch('todos/luigi.json')
-  .then((res) => {
-    console.log('resolved', res)
-    return res.json()
-  })
+const getTodos = async () => {
+  const response = await fetch('todos/luigi.json')
+  const data = await response.json()
+
+  if (response.status === 200) {
+    return data
+  } else {
+    return 'Could not fetch data'
+  }
+}
+
+console.log(1)
+console.log(2)
+getTodos()
   .then((data) => {
-    console.log(data)
+    console.log('resolved: ', data)
   })
   .catch((err) => {
-    console.log('rejected', err)
+    console.log('rejected: ', err)
   })
+console.log(3)
+console.log(4)
+
+// fetch('todos/luigi.json')
+//   .then((res) => {
+//     console.log('resolved', res)
+//     return res.json()
+//   })
+//   .then((data) => {
+//     console.log(data)
+//   })
+//   .catch((err) => {
+//     console.log('rejected', err)
+//   })
